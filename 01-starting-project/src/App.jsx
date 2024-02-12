5f7130086e8023ef832d88b42e9d5297d8499d48
amankulshreshtha <aman21102000@gmail.com>
2024-02-13 01:02:45 +0530

    adding props to components

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,16 @@
+import reactImg from "./assets/react-core-concepts.png"
+import {CORE_CONCEPTS} from "./data"
+
+function CoreComponent(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function Header() {
 
   const descriptionArray = ["Core","Crucial","Important"];
@@ -10,7 +23,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} Fundamental React concepts you will need for almost any app you are
@@ -20,12 +33,25 @@ function Header() {
   );
 }
 
+
+
 function App() {
   return (
     <div>
       <Header />
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Components</h2>
+          <ul>
+            <CoreComponent title={CORE_CONCEPTS[0].title} image={CORE_CONCEPTS[0].image} description={CORE_CONCEPTS[0].description} />
+            <CoreComponent {...CORE_CONCEPTS[1]}/>
+            <CoreComponent {...CORE_CONCEPTS[2]}/>
+            <CoreComponent {...CORE_CONCEPTS[3]}/>
+        </ul>
+        </section>
+       
+
       </main>
     </div>
   );
