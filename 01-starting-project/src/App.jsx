532590f42dcd11936f60a47e81aab1176a145a55
amankulshreshtha <aman21102000@gmail.com>
2024-02-13 00:19:43 +0530

    output dynamic value basics

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,14 +1,29 @@
+function Header() {
+
+  const descriptionArray = ["Core","Crucial","Important"];
+
+  function getRandomIndex(size) {
+    return Math.floor(Math.random()*(size));
+  }
+
+  const description = descriptionArray[getRandomIndex(descriptionArray.length)]
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
